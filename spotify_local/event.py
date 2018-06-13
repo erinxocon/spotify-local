0040e84a2388024429a842929a00202e07b27966
Erin O'Connell <erinocon5@gmail.com>
2018-06-12 23:38:18 -0600

    gotta return self

--- spotify_local/event.py
+++ spotify_local/event.py
@@ -1,17 +1,22 @@
 class Event:
     def __init__(self):
-        self.handlers: set = set()
+        self.handlers = set()
 
     def __iadd__(self, handler):
-        print(handler)
         self.handlers.add(handler)
+        return self
 
     def __isub__(self, handler):
         try:
             self.handlers.remove(handler)
         except KeyError:
             raise ValueError("Can't unhandle the handle as it's not currently handled.")
+        return self
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args, **kargs):
         for handler in self.handlers:
-            handler(*args, **kwargs)
+            handler(*args, **kargs)
+
+    def __len__(self):
+        return len(self.handlers)
+

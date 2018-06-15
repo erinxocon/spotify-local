da4da51431a5042f16830aaebb8d9c3ef8d92f7f
Erin O'Connell <erinocon5@gmail.com>
2018-06-14 22:17:58 -0600

    added methods to check if spotify is connected, slots for memory management, and check to see if they are running 3.6

--- spotify_local/core.py
+++ spotify_local/core.py
@@ -1,3 +1,5 @@
+import sys
+
 import keyboard
 
 from random import choices
@@ -10,34 +12,63 @@ from requests import session, Response, Session
 from .event import Event
 from .update_status import UpdateStatus
 
-# define types
-_KEYVALUE = Mapping[str, Union[object, str]]
+try:
+    assert sys.version_info.major == 3
+    assert sys.version_info.minor > 5
+except AssertionError:
+    raise RuntimeError("Spotify-Local-Control requires Python 3.6+!")
+
+
+DEFAULT_ORIGIN: Dict = {"Origin": "https://open.spotify.com"}
+DEFAULT_PORT: int = 4381
 
 
 class SpotifyLocal:
+
+    __slots__ = [
+        "session",
+        "_origin",
+        "_port",
+        "_connected",
+        "_oauth_token",
+        "_csrf_token",
+        "_process",
+        "on_status_change",
+    ]
+
     def __init__(self) -> None:
         self.session: Session = session()
-        self._origin: _KEYVALUE = {"Origin": "https://open.spotify.com"}
-        self._port: int = 4381
+        self._origin: Dict = DEFAULT_ORIGIN
+        self._port: int = DEFAULT_PORT
+        self._connected: bool = False
         self._oauth_token: str
         self._csrf_token: str
         self._process: Optional[Process] = None
-        self.on_change_status: Event = Event()
+        self.on_status_change: Event = Event()
 
     def __enter__(self):
-        self._oauth_token = self._get_oauth_token()
-        self._csrf_token = self._get_csrf_token()
+        self.connect()
         return self
 
     def __exit__(self, *args):
+        self.disconnect()
+
+    def connect(self) -> None:
+        self._oauth_token = self._get_oauth_token()
+        self._csrf_token = self._get_csrf_token()
+        self._connected = True
+
+    def disconnect(self) -> None:
         if self._process is not None:
             self._process.join()
+        self._connected = False
 
     def _get_url(self, url: str) -> str:
         sub = "{0}.spotilocal.com".format("".join(choices(ascii_lowercase, k=10)))
         return "http://{0}:{1}{2}".format(sub, self._port, url)
 
     def _make_request(self, url: str, params: Dict = {}) -> Response:
+
         r: Response = self.session.get(url=url, params=params, headers=self._origin)
         return r
 
@@ -51,6 +82,14 @@ class SpotifyLocal:
         r = self._make_request(url=url)
         return r.json()["token"]
 
+    def _check_if_connected(self) -> None:
+        try:
+            assert self._connected == True
+        except AssertionError:
+            raise ConnectionError(
+                "Please either use with context, or call SpotifyLocal.connect() before calling this method."
+            )
+
     @property
     def version(self):
         url: str = self._get_url("/service/version.json")
@@ -59,12 +98,14 @@ class SpotifyLocal:
         return r.json()
 
     def get_current_status(self) -> Mapping:
+        self._check_if_connected()
         url: str = self._get_url("/remote/status.json")
         params = {"oauth": self._oauth_token, "csrf": self._csrf_token}
         r = self._make_request(url=url, params=params)
         return r.json()
 
     def pause(self, pause=True) -> None:
+        self._check_if_connected()
         url: str = self._get_url("/remote/pause.json")
         params = {
             "oauth": self._oauth_token,
@@ -77,6 +118,7 @@ class SpotifyLocal:
         self.pause(pause=False)
 
     def playURI(self, uri: str) -> Mapping:
+        self._check_if_connected()
         url: str = self._get_url("/remote/play.json")
         params = {
             "oauth": self._oauth_token,
@@ -93,11 +135,16 @@ class SpotifyLocal:
     def previous(self) -> None:
         keyboard.send("previous track")
 
-    def listen_for_events(self) -> None:
+    def listen_for_events(self, wait: int = 60) -> None:
+        self._check_if_connected()
         url: str = self._get_url("/remote/status.json")
-        params = {"oauth": self._oauth_token, "csrf": self._csrf_token}
+        params: Dict = {"oauth": self._oauth_token, "csrf": self._csrf_token}
         self._process = UpdateStatus(
-            handlers=self.on_change_status, params=params, headers=self._origin, url=url
+            handlers=self.on_status_change,
+            params=params,
+            headers=self._origin,
+            url=url,
+            wait=wait,
         )
         self._process.start()
 

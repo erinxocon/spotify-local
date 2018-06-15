773b29070251699b3f5486fc529e98a128090e12
Erin O'Connell <erinocon5@gmail.com>
2018-06-14 22:19:26 -0600

    sime test

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ from spotify_local import SpotifyLocal
 
 
 def test(new_status):
-    print(new_status)
+    pprint(new_status)
 
 
 if __name__ == "__main__":
     with SpotifyLocal() as s:
-        s.on_change_status += test
-        s.listen_for_events()
+        # s.on_status_change += test
+        # s.listen_for_events()
+        pprint(s.get_current_status())
+        print("This should print out")

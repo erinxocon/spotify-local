234b077047f0be17cbe608a9a546f7871371f414
Erin O'Connell <erinocon5@gmail.com>
2018-06-14 22:19:12 -0600

    added some typing and variablized the returnafter parameter

--- spotify_local/update_status.py
+++ spotify_local/update_status.py
@@ -1,27 +1,30 @@
+import sys
+
 import requests
 
 from multiprocessing import Process, queues
-from typing import Callable, Mapping, Union
+from typing import Callable, Mapping, Union, Dict
 
 from .event import Event
 
 
 class UpdateStatus(Process):
     def __init__(
-        self, handlers: Event, params: Mapping, headers: Mapping, url: str
+        self, handlers: Event, params: Dict, headers: Dict, url: str, wait: int
     ) -> None:
-        self.params = params
-        self.headers = headers
-        self.url = url
-        self.session = requests.Session()
-        self.handlers = handlers
+        self.params: Dict = params
+        self.headers: Dict = headers
+        self.url: str = url
+        self.session: requests.Session = requests.Session()
+        self.handlers: Event = handlers
+        self.wait: int = wait
         super(UpdateStatus, self).__init__()
 
-    def run(self):
+    def run(self) -> None:
         while True:
             self.params["returnon"] = "login,logout,play,pause,error,ap"
-            self.params["returnafter"] = 60
-            r: Response = self.session.get(
+            self.params["returnafter"] = str(self.wait)
+            r: requests.Response = self.session.get(
                 url=self.url, params=self.params, headers=self.headers
             )
             j = r.json()

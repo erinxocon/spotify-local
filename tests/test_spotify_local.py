425f22d90fa8ebd6c822c0ffe69b7bc45a25a07f
Erin O'Connell <erinocon5@gmail.com>
2018-06-17 01:40:25 -0600

    await the version

--- tests/test_spotify_local.py
+++ tests/test_spotify_local.py
@@ -8,8 +8,7 @@ ioloop = asyncio.get_event_loop()
 
 async def test():
     async with SpotifyLocalAsync(loop=ioloop, workers=5) as s:
-        print(await s._get_oauth_token())
-        print(await s._get_csrf_token())
+        print(await s.version)
 
 
 ioloop.run_until_complete(test())

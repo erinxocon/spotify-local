2928989afcba92af9d3cb7852417d4afd2e8817a
Erin O'Connell <erinocon5@gmail.com>
2018-06-17 01:40:06 -0600

    got the async class fleshed out a bit more

--- src/spotify_local/core.py
+++ src/spotify_local/core.py
@@ -5,10 +5,7 @@ import aiohttp
 import keyboard
 
 from random import choices
-from functools import partial
 from string import ascii_lowercase
-from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures._base import TimeoutError
 from multiprocessing import Process, queues, get_context
 from typing import Dict, Union, Mapping, Callable, Optional
 
@@ -50,34 +47,105 @@ class SpotifyLocalAsync:
         self._origin: Dict = DEFAULT_ORIGIN
         self._loop = loop or asyncio.get_event_loop()
         self._session = aiohttp.ClientSession()
+        self._oauth_token = ""
+        self._csrf_token = ""
+        self._connected = False
 
     async def __aenter__(self):
+        await self.connect()
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
         await self.disconnect()
 
-    async def request(self, url: str):
+    async def _request(self, url: str, params: Dict = {}):
         headers = self._origin
-        async with self._session.get(url, headers=headers) as r:
+        async with self._session.get(url, headers=headers, params=params) as r:
             return await r.json()
 
     async def _get_oauth_token(self):
         """Retrieve a simple OAuth Token for use with the local http client."""
         url: str = "{0}/token".format(self._origin["Origin"])
-        print(url)
-        r = await self.request(url=url)
+        r = await self._request(url=url)
         return r["t"]
 
     async def _get_csrf_token(self) -> str:
         """Retrieve a simple csrf token for to prevent cross site request forgery."""
         url: str = get_url("/simplecsrf/token.json")
         print(url)
-        r = await self.request(url=url)
-        return r
+        r = await self._request(url=url)
+        return r["token"]
+
+    async def connect(self) -> None:
+        """Register the oauth token, the csrf token, and set connected to true"""
+        self._oauth_token = await self._get_oauth_token()
+        self._csrf_token = await self._get_csrf_token()
+        self._connected = True
 
     async def disconnect(self) -> None:
+        self._connected = False
         await self._session.close()
+
+    def _check_if_connected(self) -> None:
+        """Checks to see if we have connected to the spotify client, essentially a "isSpotifyOpen" check"""
+        try:
+            assert self._connected == True
+        except AssertionError:
+            raise ConnectionError(
+                "Please either use with context, or call SpotifyLocal.connect() before calling this method."
+            )
+
+    @property
+    async def version(self):
+        """Spotify version information"""
+        url: str = get_url("/service/version.json")
+        params = {"service": "remote"}
+        r = await self._request(url=url, params=params)
+        return r
+
+    async def get_current_status(self) -> Mapping:
+        """Returns the current state of the local spotify client"""
+        self._check_if_connected()
+        url: str = get_url("/remote/status.json")
+        params = {"oauth": self._oauth_token, "csrf": self._csrf_token}
+        r = await self._request(url=url, params=params)
+        return r
+
+    async def pause(self, pause=True) -> None:
+        """Pauses the spotify player
+        :param pause: boolean value to choose the pause/play state
+        """
+        self._check_if_connected()
+        url: str = get_url("/remote/pause.json")
+        params = {
+            "oauth": self._oauth_token,
+            "csrf": self._csrf_token,
+            "pause": "true" if pause else "false",
+        }
+        await self._request(url=url, params=params)
+
+    async def unpause(self) -> None:
+        """Unpauses the player by calling pause()"""
+        await self.pause(pause=False)
+
+    async def playURI(self, uri: str) -> Mapping:
+        """Play a Spotify uri, for example spotify:track:5Yn8WCB4Dqm8snemB5Mu4K"""
+        self._check_if_connected()
+        url: str = get_url("/remote/play.json")
+        params = {
+            "oauth": self._oauth_token,
+            "csrf": self._csrf_token,
+            "uri": uri,
+            "context": uri,
+        }
+        r = await self._request(url=url, params=params)
+        return r
+
+    def skip(self) -> None:
+        SpotifyLocal.skip()
+
+    def previous(self) -> None:
+        SpotifyLocal.previous()
 
 
 class SpotifyLocal:
@@ -197,14 +265,16 @@ class SpotifyLocal:
         r = self._request(url=url, params=params)
         return r.json()
 
-    def skip(self) -> None:
+    @staticmethod
+    def skip() -> None:
         """Skips the current song"""
         if sys.platform == "darwin":
             keyboard.send("KEYTYPE_SKIP")
         else:
             keyboard.send("next track")
 
-    def previous(self) -> None:
+    @staticmethod
+    def previous() -> None:
         """Goes to the beginning of the track, or if called twice goes to the previous track."""
         if sys.platform == "darwin":
             keyboard.send("KEYTYPE_PREVIOUS")

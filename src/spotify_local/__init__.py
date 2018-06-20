a85c6a530d2d2b115b8464757aba5f3835d56d28
Erin O'Connell <erinocon5@gmail.com>
2018-06-20 17:40:40 -0600

    version bump

--- src/spotify_local/__init__.py
+++ src/spotify_local/__init__.py
@@ -2,11 +2,11 @@ import sys
 
 try:
     assert sys.version_info.major == 3
-    assert sys.version_info.minor > 5
+    assert sys.version_info.minor > 2
 except AssertionError:
     raise RuntimeError("Spotify-Local requires Python 3.6+!")
 
 from .core import SpotifyLocal
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 

fb83b0312ac58cc6f0a312a9a6006ebb3ce0c4de
Erin O'Connell <erinocon5@gmail.com>
2018-06-19 22:48:19 -0600

    extracted token retrieval to utils module

--- src/spotify_local/utils.py
+++ src/spotify_local/utils.py
@@ -1,7 +1,11 @@
 from random import choices
 from string import ascii_lowercase
 
-from .config import DEFAULT_PORT
+from requests import Response, session, Session
+
+from .config import DEFAULT_PORT, DEFAULT_ORIGIN
+
+s: Session = session()
 
 
 def get_url(url: str) -> str:
@@ -11,3 +15,17 @@ def get_url(url: str) -> str:
     """
     sub = "{0}.spotilocal.com".format("".join(choices(ascii_lowercase, k=10)))
     return "http://{0}:{1}{2}".format(sub, DEFAULT_PORT, url)
+
+
+def get_oauth_token() -> str:
+    """Retrieve a simple OAuth Token for use with the local http client."""
+    url: str = "{0}/token".format(DEFAULT_ORIGIN["Origin"])
+    r: Response = s.get(url=url)
+    return r.json()["t"]
+
+
+def get_csrf_token() -> str:
+    """Retrieve a simple csrf token for to prevent cross site request forgery."""
+    url: str = get_url("/simplecsrf/token.json")
+    r: Response = s.get(url=url, headers=DEFAULT_ORIGIN)
+    return r.json()["token"]

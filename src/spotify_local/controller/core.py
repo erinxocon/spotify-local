f3e363dc4206e66218e5e429dc5cc27eac8bc177
Erin O'Connell <erinocon5@gmail.com>
2018-06-19 22:47:42 -0600

    added basic event emiiter support

--- src/spotify_local/controller/core.py
+++ src/spotify_local/controller/core.py
@@ -1,6 +1,9 @@
 from collections import defaultdict, OrderedDict
 from uuid import uuid4
 
+from ..utils import get_url, get_csrf_token, get_oauth_token
+from ..config import DEFAULT_ORIGIN
+
 
 class SpotifyLocal:
     def __init__(self):
@@ -15,8 +18,20 @@ class SpotifyLocal:
 
     def add_event_handler(self, event, func):
         uuid = uuid4()
-        self._registered_events[event][str(uuid)] = func
+        self._registered_events[event][func] = func
 
     def emit(self, event, *args, **kwargs):
         for func in self._registered_events[event].values():
             func(*args, **kwargs)
+
+    def remove_listener(self, event, f):
+        self._registered_events[event].pop(f)
+
+    def remove_all_listeners(self, event=None):
+        if event is not None:
+            self._registered_events[event] = OrderedDict()
+        else:
+            self._registered_events = defaultdict(OrderedDict)
+
+    def listeners(self, event):
+        return list(self._registered_events[event].keys())

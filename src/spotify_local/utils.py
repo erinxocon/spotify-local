49a1a9c7cd9ddbbc1d4cca018aa08ff5e14469f6
Erin O'Connell <erinocon5@gmail.com>
2018-06-21 12:25:36 -0600

    added two methods to check if spotify is running and if the web helper is running

--- src/spotify_local/utils.py
+++ src/spotify_local/utils.py
@@ -1,3 +1,7 @@
+import sys
+
+import psutil
+
 from random import choices
 from string import ascii_lowercase
 
@@ -8,24 +12,38 @@ from .config import DEFAULT_PORT, DEFAULT_ORIGIN
 s: Session = session()
 
 
-def get_url(url: str) -> str:
+def get_url(url):
     """Ranomdly generates a url for use in requests.
     Generates a hostname with the port and the provided suffix url provided
+
     :param url: A url fragment to use in the creation of the master url
     """
     sub = "{0}.spotilocal.com".format("".join(choices(ascii_lowercase, k=10)))
     return "http://{0}:{1}{2}".format(sub, DEFAULT_PORT, url)
 
 
-def get_oauth_token() -> str:
+def get_oauth_token():
     """Retrieve a simple OAuth Token for use with the local http client."""
-    url: str = "{0}/token".format(DEFAULT_ORIGIN["Origin"])
-    r: Response = s.get(url=url)
+    url = "{0}/token".format(DEFAULT_ORIGIN["Origin"])
+    r = s.get(url=url)
     return r.json()["t"]
 
 
-def get_csrf_token() -> str:
+def get_csrf_token():
     """Retrieve a simple csrf token for to prevent cross site request forgery."""
-    url: str = get_url("/simplecsrf/token.json")
-    r: Response = s.get(url=url, headers=DEFAULT_ORIGIN)
+    url = get_url("/simplecsrf/token.json")
+    r = s.get(url=url, headers=DEFAULT_ORIGIN)
     return r.json()["token"]
+
+
+def is_spotify_running():
+    procs = [p.name() for p in psutil.process_iter()]
+    if sys.platform == "win32":
+        return "Spotify.exe" in procs
+
+
+def is_spotify_web_helper_running():
+    procs = [p.name() for p in psutil.process_iter()]
+    if sys.platform == "win32":
+        return "SpotifyWebHelper.exe" in procs
+

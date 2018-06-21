1e07d30eb687fdde513992ece51d72fdb1ddaff6
Erin O'Connell <erinocon5@gmail.com>
2018-06-21 12:25:48 -0600

    more tests

--- tests/test_spotify_local.py
+++ tests/test_spotify_local.py
@@ -1,28 +1,36 @@
-from spotify_local import SpotifyLocal
+from spotify_local import (
+    SpotifyLocal,
+    is_spotify_running,
+    is_spotify_web_helper_running,
+)
 from time import sleep
 
-s = SpotifyLocal()
+# s = SpotifyLocal()
 
 
-@s.on("play_state_change")
-def test(status):
-    print("Play Pause Engaged")
+# @s.on("play_state_change")
+# def test(status):
+#     print("Play Pause Engaged")
 
 
-# @s.on("status_change")
-# def test1(status):
-#     print("new status")
+# # @s.on("status_change")
+# # def test1(status):
+# #     print("new status")
 
 
-@s.on("track_change")
-def test2(status):
-    print("track changed")
+# @s.on("track_change")
+# def test2(status):
+#     print("track changed")
 
 
-print(s._registered_events)
+# print(s._registered_events)
 
-s.listen(blocking=True)
+# s.listen(blocking=True)
 
-print("let's see if this prints out")
+# print("let's see if this prints out")
 
-sleep(120)
+# sleep(120)
+
+
+print(is_spotify_running())
+print(is_spotify_web_helper_running())
